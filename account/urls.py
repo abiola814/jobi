f0267bee5c8859c377dc24f1107f7a5b122230ec
gafaradetunji <gafaradetunji471@gmail.com>
2022-12-28 21:17:18 +0100

    home

--- account/urls.py
+++ account/urls.py
@@ -15,4 +15,5 @@ urlpatterns=[
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
     path('forgot_password/user_forgot_password', views.forgot_password, name='user_forgot_password'),
+    path('password_reset/done', views.password_reset_done, name='password_reset_done'),
 ]
